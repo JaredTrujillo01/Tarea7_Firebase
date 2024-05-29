6860e450cc1aa303fb06b314e4b1162a87aaeac2
Eduardo Jared Trujillo Gamero <167134639+JaredTrujillo01@users.noreply.github.com>
2024-05-29 10:17:55 -0600

    Proyecto base

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_tarea7_firebase
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_tarea7_firebase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_tarea7_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
